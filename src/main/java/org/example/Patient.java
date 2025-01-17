252f0756609421dd84a2ba073562c5b161abf6cc
Dxcraig <steviedoe20@gmail.com>
2025-01-17 21:08:48 +0000

    Initial commit

---
+++ src/main/java/org/example/Patient.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class Patient {
+    private String patientId;
+    private String name;
+    private int age;
+    private String diagnosis;
+
+
+    public Patient (String patientId, String name, int age, String diagnosis){
+        this.patientId = patientId;
+        this.name = name;
+        this.age = age;
+        this.diagnosis = diagnosis;
+    }
+
+    public String getPatientId(){
+        return patientId;
+    }
+    public String getName (){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getDiagnosis(){
+        return diagnosis;
+    }
+    public void setAge(int age){
+        if (age < 0 ){
+            throw new IllegalArgumentException("Invalid age");
+        }
+        this.age = age;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        if (diagnosis == null || diagnosis.isEmpty()){
+            throw new IllegalArgumentException("Diagnosis cannot be empty");
+        }
+        this.diagnosis = diagnosis;
+    }
+}
